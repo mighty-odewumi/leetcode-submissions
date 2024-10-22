bfc3d050000cb87153019049652a613a000f2dd6
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-10-22 22:55:43 +0100

    Time: 95 ms (97%), Space: 26 MB (97.81%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,61 @@
+class Solution:
+  def maxProfit(self, prices: List[int]) -> int:
+    """
+    Iterate through array and get min value
+    From that index of min, compare each to get max value
+    Return max value - min to get profit
+    """
+
+    """
+    totalMin = 0
+    totalMax = 0
+    index = 0
+    storeMin = {}
+    storeMax = {}
+
+    for i in range(len(prices)):
+      if (i == 0):
+        totalMin = prices[i]
+        index = i
+        storeMin[totalMin] = index
+      else:
+        totalMin = min(totalMin, prices[i])
+        if (totalMin == prices[i]):
+          index = i
+          storeMin[totalMin] = i
+
+    print("totalMin", totalMin)
+    print("storeMin", storeMin)
+
+    newPrices = prices[index:]
+    for j in range(len(newPrices)):
+      if (j == 0):
+        totalMax = newPrices[j]
+        index = j
+        storeMax[totalMax] = index
+      else:
+        totalMax = max(totalMax, newPrices[j])
+        if (totalMax == newPrices[j]):
+          index = j
+          storeMax[totalMax] = index
+    print("Total Max", totalMax)
+    print("StoreMax", storeMax)
+
+    return totalMax - totalMin
+    """
+
+    cheapest = 0
+    maxProfit = 0
+
+    for i in range(len(prices)):
+        if i == 0: 
+            maxProfit = 0
+            cheapest = prices[i]
+        
+        if prices[i] < cheapest:
+            cheapest = prices[i]
+        
+        if prices[i] > cheapest:
+            currentProfit = prices[i] - cheapest
+            maxProfit = max(maxProfit, currentProfit)
+    return maxProfit
