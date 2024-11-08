5079d511e7898d99c0c290003b3a9c19e9f5cd3f
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-11-08 18:08:06 +0100

    Time: 3 ms (68.4%), Space: 18.7 MB (44.76%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,9 +1,24 @@
 from collections import Counter
 
 class Solution:
+    """
+    This is a crude and inefficient solution in terms of space complexity.
+    Optimal method would be to use Bit Manipulation which would be later when I learn that.
+    """
     def singleNumber(self, nums: List[int]) -> int:
-        count = Counter(nums)
+        count = Counter(nums)  # Building a Counter uses O(n) time complexity
+        # because it has to go through each item given to it. 
+        # And Counter and dictionary  in general have a space complexity of O(n) 
+        # because they depend on the size of our input.
+        # Having a space complexity of O(1) would mean that we use a boolean variable for our solution 
 
-        for key, value in count.items():
-            if (value == 1): return key        
-            
+        # for key, value in count.items():
+        #     if (value == 1): return key        
+
+        # Another user's Bit Manipulation solution
+        # Definitely improved the space complexity
+        a = b = 0
+        for n in nums:
+            b = (b^n)&~a
+            a = (a^n)&~b
+        return b
