48cc9c1aad1e15bc033563f1846d990a27510845
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-11-08 16:42:50 +0100

    Time: 14 ms (17.51%), Space: 18.9 MB (69.31%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        store = {}
+
+        for index, val in enumerate(nums):
+            if store.get(val) == 1:
+                res = store.get(val)
+                res += 1
+                store[val] = res
+            else:
+                store[val] = 1
+        
+        for key, value in store.items():
+            if (value == 1):
+                return key
