af6268deb3c31f7741ef85617bf9b69d6b56fd52
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-11-08 17:13:10 +0100

    Time: 9 ms (25.16%), Space: 18.6 MB (88.76%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -3,17 +3,16 @@ from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         store = {}
+        for index, val in enumerate(nums):
+            if store.get(val) == 1:
+                res = store.get(val)
+                res += 1
+                store[val] = res
+            else:
+                store[val] = 1
 
-        # for index, val in enumerate(nums):
-        #     if store.get(val) == 1:
-        #         res = store.get(val)
-        #         res += 1
-        #         store[val] = res
-        #     else:
-        #         store[val] = 1
-
-        count = Counter(nums)
+        # count = Counter(nums)
         
-        for key, value in count.items():
+        for key, value in store.items():
             if (value == 1):
                 return key
