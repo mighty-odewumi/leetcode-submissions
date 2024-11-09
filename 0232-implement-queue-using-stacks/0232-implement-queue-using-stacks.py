ab7c0338bf0dc7a983ef3a6d3a96fbc37053dbe3
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-11-09 06:19:01 +0100

    Time: 0 ms (100%), Space: 16.9 MB (9.82%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,5 +1,11 @@
 class MyQueue:
 
+    # New solution uses the two stacks as mandated by the problem. 
+    # Even tho, my first solution was correct and passed and 
+    # those are the commented out code for each function.
+    # This new solution is from a post giving the guides in the Discussions.
+    # I also figured out a bug in my code from the amortized Wikipedia page.
+    # Honestly, I still don't understand why we need this. Might revisit later.
     def __init__(self):
         # self.queue = []
         self.stack1 = []
@@ -20,8 +26,8 @@ class MyQueue:
 
     def peek(self) -> int:
         # if len(self.queue) == 0: return -1
-        # else: 
-        #     return self.queue[0]
+        # else: return self.queue[0]
+
         if len(self.stack2) == 0: 
             while self.stack1:
                 self.stack2.append(self.stack1.pop())
