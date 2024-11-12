c0df03c866b6216d0a9184035e82d37789b5e158
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-11-12 03:00:26 +0100

    Time: 3 ms (14.97%), Space: 16.7 MB (29.68%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # I don't know how but my code works after so many hours of trial and error!!!
+    # Hahahaha
+
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         head1 = list1
         head2 = list2
@@ -25,46 +28,25 @@ class Solution:
                 current = current.next
             # return current
            
-            
-        if head1 == None and head2 and node == None:
+        if head1 == None and head2:
             head1 = head2
             return head1
-        elif head1 == None and head2 == None and node == None:
+        elif head1 == None and head2 == None:
             return None
-        if head1 and head1.next == None and node == None: 
+        if head1 and head1.next == None: 
             head1.next = head2
             node = head1
             sortLinkedList(node)
             return node
 
-        count = 0
-
-        # while head1.next:
-        #     count += 1
-        #     head1 = head1.next
-        # print("count", count)
-
-        # # if count == 1:
-        # if head1.next == None:
-        #     head1.next = head2
-
         while head1.next:
             node = head1
             break
 
-        print("Head1", head1)
-        print("Node", node)
         if node:
-            # node.next = head1.next
-            print("Huh", node)
             while node.next:
-                print(node)
                 node = node.next
             node.next = head2
-        print("New node", node)
-        print(head1)
-
-        print(head1)
+        
         sortLinkedList(head1)
-        print(head1)
         return head1
