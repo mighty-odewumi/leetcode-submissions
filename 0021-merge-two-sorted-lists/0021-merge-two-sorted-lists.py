c3efd98a258916d73ba347c2f7730235fa794a6c
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-11-12 04:37:55 +0100

    Time: 0 ms (100%), Space: 16.7 MB (6.89%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,11 +4,35 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # I don't know how but my code works after so many hours of trial and error!!!
-    # Hahahaha
-    # O(n**2) Time Complexity and O(1) space complexity! :(
-    # Off to the Discussions. 
+   
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # O(n) Time complexity solution from NeetCode
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        elif list2: 
+            tail.next = list2
+            
+        return dummy.next
+
+        
+        # My initial implementation!
+        # I don't know how but my code works after so many hours of trial and error!!!
+        # Hahahaha
+        # O(n**2) Time Complexity and O(1) space complexity! :(
+        # Off to the Discussions. 
+        """
         head1 = list1
         head2 = list2
         node = None
@@ -50,3 +74,4 @@ class Solution:
         
         sortLinkedList(head1)
         return head1
+        """
