b10b234abce4a9288595e026a7ae50bd3d601984
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-11-16 04:57:41 +0100

    Time: 12 ms (19.88%), Space: 17.2 MB (41.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        """
+        Use Two pointers at the start and end 
+        and then iterate throught the string checking for 
+        which value is not equal to the other. 
+        If the value of the start pointer is not equal to that of the end, return False.
+        At the end of the loop when the start is no more lesser than the end, return True.
+        """
         newStr = ""
         alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
         
         for char in s.lower():
             if char not in alpha.lower():
@@ -11,8 +17,7 @@ class Solution:
 
         start = 0
         end = len(newStr) - 1
-        print(newStr, start, end)
-        if len(newStr) == 0 or len(newStr) == 1: return True
+        # if len(newStr) == 0 or len(newStr) == 1: return True
 
         while start < end:
             if newStr[start] != newStr[end]:
