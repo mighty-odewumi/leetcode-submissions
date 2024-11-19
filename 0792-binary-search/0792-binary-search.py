6f74abb7b26fd63d7f26ed5845a1f537c0584fd1
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-11-19 03:02:46 +0100

    Time: 0 ms (100%), Space: 17.8 MB (55.72%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,5 +1,19 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target in nums: return nums.index(target)
-        else: return -1
-        
+        # First attempt using an inefficient method.
+        # if target in nums: return nums.index(target)
+        # else: return -1
+
+        # Efficient Binary Search
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]: 
+                return mid
+            elif target < nums[mid]:
+                high = mid - 1
+            elif target > nums[mid]: 
+                low = mid + 1
+        return -1
