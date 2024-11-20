f561f85fab262f2c8d902e85a0dd121982a16346
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-11-20 11:49:51 +0100

    Time: 0 ms (100%), Space: 17.9 MB (52.19%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,23 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # First attempt using an inefficient method.
-        # if target in nums: return nums.index(target)
-        # else: return -1
+        if target in nums: return nums.index(target)
+        else: return -1
 
         # Efficient Binary Search
-        low = 0
-        high = len(nums) - 1
+        # low = 0
+        # high = len(nums) - 1
 
-        # while low <= high:
-        #     mid = (low + high) // 2
-        #     if target == nums[mid]: 
-        #         return mid
-        #     elif target < nums[mid]:
-        #         high = mid - 1
-        #     elif target > nums[mid]: 
-        #         low = mid + 1
-        # return -1
+        # Iterative method
+        """
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]: 
+                return mid
+            elif target < nums[mid]:
+                high = mid - 1
+            elif target > nums[mid]: 
+                low = mid + 1
+        return -1
+        """
 
+        # Recursive Method
+        """
         def binarySearch(nums, low, high):
 
             mid = (low + high) // 2
@@ -28,3 +33,4 @@ class Solution:
             elif target < nums[mid]: return binarySearch(nums, low, mid - 1)
 
         return binarySearch(nums, low, high)
+        """
